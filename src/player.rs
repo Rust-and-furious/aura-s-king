b02ef2fca0042eae3afa0559ea06fcff0f85f7da
Quentin <232542381+QuentinGarnier-uapv@users.noreply.github.com>
2026-05-04 09:38:08 +0200

    Update player.rs

--- src/player.rs
+++ src/player.rs
@@ -2,6 +2,6 @@ use crate::entities::Objet;
 
 pub struct Player {
     pub aura: f64,
-    pub zone: usize, // Représente la relation "se trouve dans" (1 Zone)
-    pub inventory: Vec<Objet>, // Représente la relation "possède (inventaire)" (0..* Objet)
+    pub zone: usize,
+    pub inventory: Vec<Objet>,
 }
